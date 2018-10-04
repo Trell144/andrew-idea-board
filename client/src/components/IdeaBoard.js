6104847b35fb3b3c363b079c3d83fd425ae872d6
Catrell Matthews <catrellmatthewss@gmail.com>
2018-10-04 17:29:41 -0400

    bugfix

--- client/src/components/IdeaBoard.js
+++ client/src/components/IdeaBoard.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
-import { reverse } from 'dns';
 
 const StyledPageWrapper = styled.div`
   display: flex;
