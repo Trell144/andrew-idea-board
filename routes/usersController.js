9b4123bdcd557fa4cff1bf89b39dcb3ed1644869
Catrell Matthews <catrellmatthewss@gmail.com>
2018-10-04 12:39:10 -0400

    end of lesson

--- routes/usersController.js
+++ routes/usersController.js
@@ -6,4 +6,41 @@ router.get('/', async (req, res) => {
   res.send(response)
 })
 
+// router.get('/', (req, res) => {
+//   user.find
+//   .then((response) => {
+//     res.send(response)
+// })
+// })
+
+
+
+//SHOW ONE
+
+router.get('/', async (req, res) => {
+  const user = await User.findById(req, params.id)
+  res.send(user)
+})
+
+
+//CREATE
+router.post('/', async (req, res) => {
+  const user = await User.create(req.body)
+  res.send(user)
+
+})
+
+//UPDATE
+router.put('/:id', async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  res.send(user)
+})
+
+//DELETE
+router.delete('/:id', async (req, res) => {
+  await User.findByIdAndUpdateRemove(req, params.id)
+  res.sendStatus(200)
+})
+
+
 module.exports = router
