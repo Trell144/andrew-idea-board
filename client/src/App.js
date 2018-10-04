ec527acc2b6cce0a9c83e4c4caa7995e58a5f26b
danielpinoga <daniel.pino@generalassemb.ly>
2018-10-04 09:50:11 -0400

    home page works!

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
-import IdeaBoard from './components/IdeaBoard';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import IdeaBoard from './components/IdeaBoard'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <IdeaBoard />
-      </div>
-    );
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Home} />
+          <Route exact path='/user/:userId' component={IdeaBoard} />
+        </Switch>
+      </Router>
+    )
   }
 }
 
